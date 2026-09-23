cc2432abdd869f4988b620cbce4cdc0711a03ece
EduardoADSP1B <eduardofranciscofirmedasilva98@gmail.com>
2026-09-23 18:47:31 -0300

    Rename Q.3.cpp to Q.03.cpp

--- Q.03.cpp
+++ Q.03.cpp
@@ -4,14 +4,14 @@
 int main() {
     float nota, soma = 0, media;
 
-	//Declaro a quantidade do for, e faço a contagem subindo com o (i++).
+	//Declaro a quantidade do for, e faÃ§o a contagem subindo com o (i++).
     for (int i = 1; i <= 10; i++) {
         printf("Digite a nota do cliente %d (0 a 10): ", i);
         scanf("%f", &nota);
         soma += nota;
     }
 	
-	//Agora faço as réguas, máxima e minimas.
+	//Agora faÃ§o as rÃ©guas, mÃ¡xima e minimas.
     media = soma / 10.0;
     printf("Media geral do atendimento: %.2f\n", media);
 
